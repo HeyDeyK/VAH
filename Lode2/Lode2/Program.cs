eb8f5f43650b68f21ddf7a8704af7fbee4609303
vileton15 <vileton15@sps-prosek.cz>
2017-09-21 12:32:59 +0200

    Konec hodiny

--- Lode2/Lode2/Program.cs
+++ Lode2/Lode2/Program.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace Lode2
 {
+    enum Typy
+    {
+        Minuto, Lod
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("JSEM GOD");
+            //Typy moznost = Typy.Prazdno;
+            //Typy[] more = new Typy[3];
+
+
+            int[,] PoleHrac1 = new int[10, 10];
+            int[,] PoleHrac2 = new int[10, 10];
+
+            PoleHrac1[1, 1] = 1;
+            PoleHrac1[1, 2] = 2;
+            if ((int)Typy.Minuto==PoleHrac1[1,3])
+            {
+                Console.WriteLine("Vedle!");
+            }
+            else
+            {
+                Console.WriteLine("Trefa!");
+            }
+
+            
+            Console.WriteLine(PoleHrac1[0,0 ]);
             Console.ReadLine();
         }
     }
